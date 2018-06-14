65c368a7558d177f95166edb30327d659f3bfb32
Josiah Witt <josiah@witt.life>
2018-06-13 20:55:33 -0400

    Add version to tag message schema

--- schema/tag.js
+++ schema/tag.js
@@ -3,9 +3,10 @@ const { msgIdRegex } = require('ssb-ref')
 module.exports = {
   $schema: 'http://json-schema.org/schema#',
   type: 'object',
-  required: ['type'],
+  required: ['type', 'version'],
   properties: {
     type: { type: 'string', pattern: 'tag' },
+    version: { type: 'integer', minimum: 1 },
     tagged: { type: 'boolean' },
     message: { type: 'string', pattern: msgIdRegex },
     root: { type: 'string', pattern: msgIdRegex },
